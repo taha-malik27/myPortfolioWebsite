59e38f9f8cc918deba4b5058d293099a4af85147
30204169 <taha.malik2@ucalgary.ca>
2025-11-15 22:30:53 -0700

    made some good progress on the homepage, added in synthwave animation, from gif, introduction/title, and making a pretty cool button purely from html css

--- myportfoliowebsite/src/app/page.tsx
+++ myportfoliowebsite/src/app/page.tsx
@@ -1,6 +1,7 @@
 import SideBarComponent from '@/components/cards/sideBar';
 import React from 'react'
-
+import Image from "next/image";
+import { backgroundBlurriness } from 'three/tsl';
 
 
 function HomePage() {
@@ -25,8 +26,34 @@ function HomePage() {
 
             {/* Right side: main content */}
             <div className = {"gradient"} style={{ padding: "1rem", color: "white"}}>
-                    <h1 className='header-styling'>Home Page</h1>
-                    <p className='subheader-styling'>Subheader - Whatever content you want here.</p>
+                    
+                    <div style={{marginTop:"5%", display:"grid", justifyContent: "center", justifyItems:"center", textAlign:"center"}}>
+
+                      <Image src={"/images/SynthSun.gif"} alt = "Sun" width={200} height={200} ></Image>
+                      
+                      <div style={{backgroundColor:"white", height: "1.5px", width:"75%"}}></div>
+
+                      <h1 className='header-styling' style={{marginBottom:"10px"}}>Hey there! I'm Taha!</h1>
+                      <p className='interim-header-styling'>Software Engineer<br/>Finance & Investing Ethusiast<br/>Synthwave & Cyberpunk Enjoyer </p>
+                      <p className='subheader-styling'>Computer Science and Finance Student at the University of Calgary</p>
+
+                      
+                      <div className="contact-me-button-container" style={{position: "relative", display: "inline-block"}}>
+                        
+                        <div className="contact-me-button-background" style={{position: "absolute", top: 0, left: 0, pointerEvents: "none"}}></div>
+                        
+                          <button className = "contact-me-button" style={{position: "relative"}}>
+                          
+                          <span className='contact-me-button-text'>Contact Me</span>
+
+                        </button>
+                      
+                      </div>
+
+
+                    </div>
+                    
+                    
             </div>
 
         </div>
