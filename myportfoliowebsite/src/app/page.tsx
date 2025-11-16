48c23cda8d7ce4a685bdf8330bf886da51edae00
30204169 <taha.malik2@ucalgary.ca>
2025-11-15 23:06:16 -0700

    added in some more css and added in interactive logo at bottom. next will be adding in interactive particles in background

--- myportfoliowebsite/src/app/page.tsx
+++ myportfoliowebsite/src/app/page.tsx
@@ -1,10 +1,16 @@
+"use client"
+
 import SideBarComponent from '@/components/cards/sideBar';
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "next/image";
 import { backgroundBlurriness } from 'three/tsl';
+import { useRouter } from 'next/navigation';
 
 
 function HomePage() {
+    const router = useRouter();
+    const [hoveredItem,setHoveredItem] = useState<string|null>(null)
+
     return (
         <div
           style={{
@@ -37,19 +43,59 @@ function HomePage() {
                       <p className='interim-header-styling'>Software Engineer<br/>Finance & Investing Ethusiast<br/>Synthwave & Cyberpunk Enjoyer </p>
                       <p className='subheader-styling'>Computer Science and Finance Student at the University of Calgary</p>
 
+                      <br/>
                       
                       <div className="contact-me-button-container" style={{position: "relative", display: "inline-block"}}>
                         
                         <div className="contact-me-button-background" style={{position: "absolute", top: 0, left: 0, pointerEvents: "none"}}></div>
                         
-                          <button className = "contact-me-button" style={{position: "relative"}}>
-                          
-                          <span className='contact-me-button-text'>Contact Me</span>
-
-                        </button>
+                          <button 
+                            className = "contact-me-button" 
+                            style={{position: "relative"}}
+                            onClick={() => router.push('/contact')}
+                          >
+                            <span className='contact-me-button-text'>Contact Me</span>
+                          </button>
                       
                       </div>
+                      <br/>
 
+                      <div
+                        onMouseEnter={() => setHoveredItem("hovering")}
+                        onMouseLeave={() => setHoveredItem(null)}
+                        style={{position: "relative", width: "70px", height: "70px"}}
+                      >
+                        
+                        <Image 
+                          src="/images/LogoWhite.png" 
+                          alt="TM" 
+                          width={70} 
+                          height={70}
+                          style={{
+                            position: "absolute", 
+                            top: 0, 
+                            left: 0,
+                            opacity: hoveredItem === "hovering" ? 0 : 1,
+                            transition: "opacity 0.3s ease-in-out"
+                          }}
+
+                        />
+                        
+                        <Image 
+                          src="/images/Logo.png" 
+                          alt="TM" 
+                          width={68} 
+                          height={68}
+                          style={{
+                            position: "absolute", 
+                            top: 0, 
+                            left: 0,
+                            opacity: hoveredItem === "hovering" ? 1 : 0,
+                            transform: "translate(0.75px, -0.25px)",
+                            transition: "opacity 0.3s ease-in-out"
+                          }}
+                        />
+                      </div>
 
                     </div>
                     
