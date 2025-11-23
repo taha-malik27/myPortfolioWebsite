bc5df035e0224d150efb941153bb3fef621284c6
30204169 <taha.malik2@ucalgary.ca>
2025-11-23 16:04:37 -0700

    structure is done, now to fill in the details and also information on expeirence

--- myportfoliowebsite/src/components/cards/WorkCard.tsx
+++ myportfoliowebsite/src/components/cards/WorkCard.tsx
@@ -51,16 +51,21 @@ const WorkCard: React.FC<WorkCardProps> = ({ backgroundColor = "transparent" }) 
                 width: "100%",
                 boxSizing: "border-box",
                 height:"100%",
-                backgroundColor:"darkblue"
-                
+                backgroundColor:"darkblue",
+                display:"flex",
+                flexDirection:"column"
             }}>
 
-            {/* Header - Matches other pages */}
-            <h1 style={{margin: 0, marginTop: "2rem", marginBottom: "1rem"}}>
+                {/* Header - Matches other pages */}
+                <h1 style={{margin: 0, marginTop: "2rem", marginBottom: "1rem"}}>
 
-                    <HoverText text="My Work Expierience" className="header-styling" />
+                        <HoverText text="My Work Expierience" className="header-styling" />
 
-            </h1>
+                </h1>
+
+                <div style={{height:"100%", width: "90%", marginBottom:"1.5rem", display: "flex", justifyContent:"center", backgroundColor:"yellowgreen"}}>
+                    Dynamic Work Experience SubCard goes, here fits till 
+                    </div>
 
 
 
@@ -93,10 +98,11 @@ const WorkCard: React.FC<WorkCardProps> = ({ backgroundColor = "transparent" }) 
                     gridAutoRows: "1fr"
                 }}>
 
+                    <div style={{border:"black 1px solid"}}>1</div>
+                    <div style={{border:"black 1px solid"}}>2</div>
                     <div style={{border:"black 1px solid"}}>3</div>
-                    <div style={{border:"black 1px solid"}}>3</div>
-                    <div style={{border:"black 1px solid"}}>3</div>
-                    <div style={{border:"black 1px solid"}}>3</div>
+                    <div style={{border:"black 1px solid"}}>4</div>
+                    <div style={{border:"black 1px solid"}}>5</div>
                     
                 </div>
 
@@ -104,7 +110,14 @@ const WorkCard: React.FC<WorkCardProps> = ({ backgroundColor = "transparent" }) 
                 <div style={{border:"black 1px solid",
                     display:"grid",
                     gridAutoRows: "1fr"
-                }}>2</div>
+                }}>
+                    
+                    <div className='vertical-line'>
+                        
+                        
+                        </div>
+                    
+                </div>
 
 
                 <div style={{border:"black 1px solid",
@@ -112,10 +125,11 @@ const WorkCard: React.FC<WorkCardProps> = ({ backgroundColor = "transparent" }) 
                     gridAutoRows: "1fr"
                 }}>
                     
-                    <div style={{border:"black 1px solid"}}>3</div>
-                    <div style={{border:"black 1px solid"}}>3</div>
-                    <div style={{border:"black 1px solid"}}>3</div>
-                    <div style={{border:"black 1px solid"}}>3</div>
+                    <div style={{border:"black 1px solid"}}>6</div>
+                    <div style={{border:"black 1px solid"}}>7</div>
+                    <div style={{border:"black 1px solid"}}>8</div>
+                    <div style={{border:"black 1px solid"}}>9</div>
+                    <div style={{border:"black 1px solid"}}>10</div>
 
                 </div>
 
